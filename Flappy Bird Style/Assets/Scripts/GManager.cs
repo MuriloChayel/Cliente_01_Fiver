757dc9aba2f8047c87b9580211e9ac64db0d6d11
Murilo Chayel <murilochayel24@gmail.com>
2021-05-18 10:19:45 -0300

    menus

--- Flappy Bird Style/Assets/Scripts/GManager.cs
+++ Flappy Bird Style/Assets/Scripts/GManager.cs
@@ -8,24 +8,31 @@ public class GManager : MonoBehaviour
     public GameObject gameOverCanvas;
     public GameObject StartCanvas;
     public GameObject pontuation;
+    public GameObject spawnPoint;
+    public GameObject startButton;
+
     [SerializeField] TMP_Text score;
     [SerializeField] TMP_Text best;
     Scene currentScene;
-
+    
     private void Start()
     {
-        Time.timeScale = 0;
+        //Time.timeScale = 0;
         currentScene = SceneManager.GetActiveScene();
     }
     public void StartGame()
     {
-        StartCanvas.SetActive(false);
-        pontuation.SetActive(true);
         Time.timeScale = 1;
+        StartCanvas.SetActive(false);
+        startButton.SetActive(false);
+        pontuation.SetActive(true);
+        spawnPoint.SetActive(true);
+        //Time.timeScale = 1;
     }
     public void GameOver()
     {
         Time.timeScale = 0;
+        //spawnPoint.SetActive(false);
         gameOverCanvas.SetActive(true);
     }
     public void RestartLevel(string newScore)
